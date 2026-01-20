558d7f1caf965643e207e348ca770d8e124dcba5
HeshanSandeepanaJayasinghe <heshansandeepanajayasinghe@gmail.com>
2026-01-20 21:00:49 +0530

    initiated Setup and TearDown

---
+++ CSE2522_Automation/TestBase.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSE2522_Automation
+{
+    public class TestBase
+    {
+        protected IWebDriver Driver;
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://uitestingplayground.com/");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
