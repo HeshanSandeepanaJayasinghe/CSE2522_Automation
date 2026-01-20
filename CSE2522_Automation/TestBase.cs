fede8f1031013a96eeba8d36b4d43a3ae31098da
HeshanSandeepanaJayasinghe <heshansandeepanajayasinghe@gmail.com>
2026-01-20 22:45:24 +0530

    test added

--- CSE2522_Automation/TestBase.cs
+++ CSE2522_Automation/TestBase.cs
@@ -10,7 +10,12 @@ namespace CSE2522_Automation
         [SetUp]
         public void SetUp()
         {
-            Driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            options.AddArgument("--remote-allow-origins=*");      // already needed for Chrome144+
+            options.AddArgument("--ignore-certificate-errors");  // ALLOWS unsafe HTTPS
+            options.AddArgument("--allow-insecure-localhost");   // optional if localhost
+
+            Driver = new ChromeDriver(options);
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl("https://uitestingplayground.com/");
         }
---
+++ CSE2522_Automation/test/Smoke.cs
@@ -0,0 +1,24 @@
+﻿//
+// this is just a test file to check chrome
+//
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSE2522_Automation.test
+{
+    [TestFixture]
+    public class SmokeTest
+    {
+        [Test]
+        public void OpenChromeTest()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--remote-allow-origins=*");
+
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://google.com");
+            driver.Quit();
+        }
+    }
+}
